cea4fb350286cf005ce097394accc147e384ccc1
DamianoCasolari <damianocasolari@gmail.com>
2023-03-07 14:14:14 +0100

    add 3 var + result

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+let first_name = prompt("inserisci il tuo nome: ");
+console.log(first_name);
+let last_name = prompt("inserisci il tuo cognome: ");
+console.log(last_name);
+let favorite_color = prompt("inserisci il tuo colore preferito: ");
+console.log(favorite_color);
+
+document.getElementById("result").innerHTML = `${first_name}${last_name}${favorite_color}23`
+
+
